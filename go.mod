a1edf6de1fb84a48c16eadae70eacaf16b7979e5
iggy <iggynelo@gmail.com>
2025-08-23 22:39:31 +0900

    feat: installed chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/iggyray/go-server
 
 go 1.24.5
+
+require github.com/go-chi/chi/v5 v5.2.2
--- main.go
+++ main.go
@@ -1,32 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 
-	"github.com/iggyray/go-server/internal/service"
+	"github.com/go-chi/chi/v5"
+	router "github.com/iggyray/go-server/internal/http"
 )
 
 func main() {
-	http.HandleFunc("/post", postHandler)
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello!"))
+	})
+	r.Mount("/posts", router.PostRouter())
 
 	log.Printf("Listening on http://localhost:%d\n", 8000)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	postService := service.NewPostService()
-
-	post := postService.GetPost()
-
-	log.Printf("%+v", post)
-
-	_, err := io.WriteString(w, fmt.Sprintf("%+v", post))
-	if err != nil {
-		log.Println(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
